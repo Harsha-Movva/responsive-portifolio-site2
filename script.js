94708c477503d7367c7515328cf6aeb1c2e0c5d0
Harsha-Movva <munnamovva5@gmail.com>
2025-09-30 21:56:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    document.getElementById("formMessage").innerText = "Thanks! Your message has been sent.";
-    this.reset();
-});
